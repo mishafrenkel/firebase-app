1fb31bfdf3db8c33ccc2636e6f42792478fa1207
Michael Frenkel <michael.frenkel@outlook.com>
2019-10-21 19:54:54 -0700

    add HoC for session handling for conciseness

---
+++ src/components/Session/withAuthentication.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const withAuthentication = Component => {
+  class WithAuthentication extends React.Component {
+    render() {
+      return <Component {...this.props} />;
+    }
+  }
+  return WithAuthentication;
+};
+
+export default withAuthentication;
