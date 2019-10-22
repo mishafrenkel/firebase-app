5c80bf2987277b5fdafc7c9a5232c662de59290e
Michael Frenkel <michael.frenkel@outlook.com>
2019-10-21 18:57:11 -0700

    add signup logic

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+import FirebaseContext from './components/Firebase/context';
+
+ReactDOM.render(
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
+);
 
 if (module.hot) {
   module.hot.accept();
