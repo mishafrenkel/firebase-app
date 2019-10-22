d56be5057bb7b266d94b60ade817c845bbf955d7
Michael Frenkel <michael.frenkel@outlook.com>
2019-10-21 19:25:44 -0700

    add recompose for rereouting after signup/registration

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
@@ -18,9 +17,9 @@ const INITIAL_STATE = {
   passwordOne: '',
   passwordTwo: '',
   error: null,
-}
+};
 
-class SignUpFormBase extends React.Component {
+class SignUpFormBase extends Component {
   constructor(props) {
     super(props);
 
@@ -34,17 +33,18 @@ class SignUpFormBase extends React.Component {
       .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
         this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
       })
       .catch(error => {
         this.setState({ error });
-      })
+      });
 
     event.preventDefault();
-  }
+  };
 
   onChange = event => {
-    this.setState({ [event.target.name]: event.target.value })
-  }
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
   render() {
     const {
@@ -76,13 +76,15 @@ class SignUpFormBase extends React.Component {
           onChange={this.onChange}
           type="text"
           placeholder="Email Address"
-        /> <input
+        />
+        <input
           name="passwordOne"
           value={passwordOne}
           onChange={this.onChange}
           type="password"
           placeholder="Password"
-        /> <input
+        />
+        <input
           name="passwordTwo"
           value={passwordTwo}
           onChange={this.onChange}
@@ -92,6 +94,7 @@ class SignUpFormBase extends React.Component {
         <button disabled={isInvalid} type="submit">
           Sign Up
         </button>
+
         {error && <p>{error.message}</p>}
       </form>
     );
@@ -103,9 +106,6 @@ const SignUpLink = () => (
     Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
   </p>
 );
-
-const SignUpForm = withFirebase(SignUpFormBase);
-
+const SignUpForm =  withRouter(withFirebase(SignUpFormBase));
 export default SignUpPage;
-
-export { SignUpForm, SignUpLink }
+export { SignUpForm, SignUpLink };
