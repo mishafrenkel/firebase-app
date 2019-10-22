45184547b8a18e4f6857682bdb5c554c2b9748c3
Michael Frenkel <michael.frenkel@outlook.com>
2019-10-21 19:58:38 -0700

    seperate authLogic and refactor App to stateless

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -15,53 +15,27 @@ import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
 import { withFirebase } from '../Firebase';
-import { AuthUserContext } from '../Session';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      authUser: null,
-    };
-
-  }
-
-  componentDidMount() {
-    this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
-      authUser
-        ? this.setState({ authUser })
-        : this.setState({ authUser: null });
-    });
-  }
-
-  componentWillUnmount() {
-    this.listener();
-  }
-
-  render() {
-    return (
-      <AuthUserContext.Provider value={this.state.authUser}>
-        <Router>
-          <div>
-            <Navigation authUser={this.state.authUser} />
-
-            <hr />
-
-            <Route exact path={ROUTES.LANDING} component={LandingPage} />
-            <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-            <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-            <Route path={ROUTES.HOME} component={HomePage} />
-            <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-            <Route path={ROUTES.ADMIN} component={AdminPage} />
-          </div>
-        </Router>
-      </AuthUserContext.Provider>
-    );
-  }
-}
+import { withAuthentication } from '../Session';
 
 
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+      <hr />
 
-export default withFirebase(App);
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route
+        path={ROUTES.PASSWORD_FORGET}
+        component={PasswordForgetPage}
+      />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+
+    </div>
+  </Router>
+);
+export default withAuthentication(App);
